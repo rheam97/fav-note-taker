91f7a7291fb521882e07de6fcd1342fce1555da4
Rhea <rheam97@gmail.com>
2022-02-07 21:58:47 -0500

    fixed post and added delete

--- apiRoutes/apiRoutes.js
+++ apiRoutes/apiRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const { v4: uuidv4 } = require('uuid')
-const db = require('../db/db.json')
+let db = require('../db/db.json')
 const fs = require('fs')
 
 
@@ -9,6 +9,23 @@ router.get('/api/notes', (req, res)=> {
 })
 
 router.post('/api/notes', (req, res)=> {
-    
+    req.body.id = uuidv4()
+   const {title, text, id} = req.body
+    const newNote ={
+        title,
+        text,
+        id
+    }
+        db.push(newNote)
+        fs.writeFileSync('db/db.json', JSON.stringify(db))
+        res.json(db)
+})
+
+router.delete('/api/notes/:id', (req, res)=> {
+    const gone = req.params.id
+    db = db.filter(note=> note.id!== gone)
+    fs.writeFileSync('db/db.json', JSON.stringify(db))
+    res.json(db)
+
 })
 module.exports = router
